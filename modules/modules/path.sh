dbdac1e408170638e92440c11a4d5e000392cd72
tmarkov <noreply@tmarkov.com>
2023-07-31 21:10:13 +0300

    Resolve symlinked folders in PATH (nix)

--- modules/modules/path.sh
+++ modules/modules/path.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find ${PATH//:/ } -maxdepth 1 -executable -printf "man$SEP%f$SEP;c %f\n" 2> /dev/null
+find -L ${PATH//:/ } -maxdepth 1 -executable -not -name ".*" -printf "man$SEP%f$SEP;c %f\n" 2> /dev/null
